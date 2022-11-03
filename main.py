16c8f06f53b3dda9aad02f48d0de221cc67540a8
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2022-11-03 19:07:59 +0300

    Initial solution

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    data = {
-        "slackUsername": "mitsuki",
-        "backend": True,
-        "age": 22,
-        "bio": "I love computers and anime"
-        }
-    return(jsonify(data))
+@app.errorhandler(404)
+def invalid_route(e):
+    return "Invalid route."
 
+
+@app.route('/', methods=["POST"])
+def index():
+    operation = request.json["operation_type"]
+    x = request.json["x"]
+    y = request.json["y"]
+
+    response = {
+        "slackUsername": "mitsuki",
+        "operation_type": operation
+    }
+
+    if operation == "addition":
+        response.update({ "result": x + y })
+    elif operation == "subtraction":
+        response.update({ "result": x - y })
+    elif operation == "multiplication":
+        response.update({ "result": x * y })
+    else:
+        return "Invalid operation."
+
+    return response
+    
 
 if __name__ == "__main__":
     app.run(port=os.getenv("PORT", default=5000))
